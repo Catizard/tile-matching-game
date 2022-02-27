0de64da90d82ddb7febb1da69c1299cbaafa54f4
Dreamfarer <1185032459@qq.com>
2022-02-27 09:12:24 +0800

    import redis

---
+++ src/main/java/com/dreamtea/Utils/Status.java
@@ -0,0 +1,44 @@
+package com.dreamtea.Utils;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 状态枚举
+ * @program: dgpoms-server-root
+ * @Date: 2018/12/27 11:40
+ * @Author: Mr.Zheng
+ * @Description:
+ */
+public abstract  class Status {
+
+    /**
+     * 过期时间相关枚举
+     */
+    public enum ExpireEnum{
+        //未读消息的有效期为30天
+        UNREAD_MSG(30L, TimeUnit.DAYS)
+        ;
+
+        /**
+         * 过期时间
+         */
+        private final Long time;
+        /**
+         * 时间单位
+         */
+        private final TimeUnit timeUnit;
+
+        ExpireEnum(Long time, TimeUnit timeUnit) {
+            this.time = time;
+            this.timeUnit = timeUnit;
+        }
+
+        public Long getTime() {
+            return time;
+        }
+
+        public TimeUnit getTimeUnit() {
+            return timeUnit;
+        }
+    }
+}
